38034f70599e815e17308c508c5d904dfe337fd5
serwus701 <serwus701@gmail.com>
2024-11-24 23:05:42 +0100

    add only countries feature + fixes

--- src/app/components/PersonScreenSide.tsx
+++ src/app/components/PersonScreenSide.tsx
@@ -2,24 +2,46 @@ import React from "react";
 import Image from "next/image";
 
 interface PersonScreenSideProps {
-    percentage: number;
     name: string;
+    panelSide: 'L' | 'R';
+    yourCountries: string[];
+    oponentCountries: string[];
+    allCountriesNumber: number;
 }
 
-export const PersonScreenSide: React.FC<PersonScreenSideProps> = ({ percentage, name }) => {
+export const PersonScreenSide: React.FC<PersonScreenSideProps> = ({ name, panelSide, yourCountries, oponentCountries, allCountriesNumber }) => {
+    const percentage = (yourCountries.length / allCountriesNumber) * 100;
+    const onlyYourtCountries = yourCountries.filter(country => !oponentCountries.includes(country));
+
     return (
         <div className="w-2/5 flex items-center justify-start mx-5 flex-col">
             <h1 className="text-5xl my-20">{name}</h1>
             <div className="flex flex-row items-center justify-center">
-                <Image src="/test.png" alt="" className="w-72 h-72 mx-5" width={500} height={500} />
-                < div className="w-20 h-72 border border-gray-300 bg-gray-100 relative" >
+                {panelSide === 'R' && (
+                    <Image src="/test.png" alt="" className="w-72 h-72 mx-5" width={500} height={500} />
+                )}
+                <div className="w-24 h-72 border border-gray-300 bg-gray-100 relative flex items-center justify-center">
                     <div
                         className="bg-green-500 absolute bottom-0 w-full"
                         style={{ height: `${percentage}%` }}
                     />
-                </ div>
+                    <span className="absolute text-black font-bold">
+                        {percentage}%
+                    </span>
+                </div>
+                {panelSide === 'L' && (
+                    <Image src="/test.png" alt="" className="w-72 h-72 mx-5" width={500} height={500} />
+                )}
             </div>
 
-        </div >
+            <div className="w-full h-40 overflow-y-auto mt-8">
+                <h2 className="text-2xl mb-2">{`W tych krajach tylko ty był${name === 'Dorota' ? 'a' : 'e'}ś:`}</h2>
+                <ul className="space-y-2">
+                    {onlyYourtCountries.map((country, index) => (
+                        <li key={index} className="text-lg">{country}</li>
+                    ))}
+                </ul>
+            </div>
+        </div>
     );
-}
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
 import { PersonScreenSide } from './components/PersonScreenSide';
+
+
+const countries1 = [
+  'polska',
+  'niemcy',
+  'francja',
+]
+
+const countries2 = [
+  'rosja',
+  'niemcy',
+  'francja',
+  'tailandia'
+]
+
+const allcountriesnumber = 10
+
+
 export default function Home() {
   return (
-    <div className="bg-white w-full h-screen text-black flex justify-center items-start">
-      <PersonScreenSide name="Dorota" percentage={75} />
-      <PersonScreenSide name="Jaś" percentage={25} />
+    <div className="bg-white w-full h-screen text-black flex flex-col justify-start items-center">
+
+      <h1 className='text-6xl mt-5'>Wyścig szczurów - edycja uzależnieni</h1>
+      <div className='flex flex-row justify-center'>
+        <PersonScreenSide name="Dorota" panelSide='R' yourCountries={countries1} oponentCountries={countries2} allCountriesNumber={allcountriesnumber} />
+        <PersonScreenSide name="Jaś" panelSide='L' yourCountries={countries2} oponentCountries={countries1} allCountriesNumber={allcountriesnumber} />
+      </div>
+      <div className='w-full justify-center flex'>
+        <h1 className='text-2xl mt-10'>Odwiedzone państwa</h1>
+      </div>
     </div>
   );
 }
